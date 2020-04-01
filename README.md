8dd13fd5922253822122ca64b4ecb8b9a7f05d70
Safiamoon <msellek.safia@gmail.com>
2020-04-01 16:16:31 +0200

    magic 8 ball

--- README.md
+++ README.md
@@ -1,19 +1,11 @@
-###  JAVASCRIPT - MOVE OBJECTS WITH ARROW KEYS - EVENTS
-###  Pacman Example : 
+###  Magic 8 Ball Example : 
 
 #### TO DO :
+     - load an 8 ball with a text field
+     - if entered, the text field disappears, and a response appears
+        (random message among 10 messages for example).
+        see: Math.random ()
+     - shake effect on the 8ball
 
- - upload an image
- - arrows direct your pacman
+ 
 
- - Bonus:
-    - add limits so that pacman does not exceed the screen
-    - smoother movement using the css transition
-    - transform the image so that pacman looks towards the direction where he is going
-
-#### You need to : 
-
-    1- start by placing your image and positioning it in absolute CSS
-    2- add top and left properties to 0px in JS when loading
-    3- detect the pressing of keys on the keyboard on yesterday's model
-    4- change the properties left and top according to pressing these keys
--- js/main.js
+++ js/main.js
@@ -1,34 +1,27 @@
-var posX=0;
-var posY=0;
-var pacmanWidth=100;
-var pacmanHeight=100;
-var directionX=1;
-var angle="0deg";
-var pacman = document.querySelector("#pacman");
-pacman.style.width=pacmanWidth+"px";
-pacman.style.height=pacmanHeight+"px";
-pacman.style.transition="left 500ms,top 500ms";
-deplacement();
-
-document.body.onkeyup=function(e){
-
-    //To know the key codes you can use "e.keyCode"
-    //console.log(e.keyCode);
-    if(e.keyCode==37){posX-=50;directionX=-1;angle="0deg";}     //left
-    if(e.keyCode==38){posY-=50;directionX=1;angle="-90deg";}    //up
-    if(e.keyCode==39){posX+=50;directionX=1;angle="0deg";}      //right
-    if(e.keyCode==40){posY+=50;directionX=1;angle="90deg";}     //down
-
-    //GESTION DES BORNES
-    if(posX<0){posX=0;}//left
-    if(posY<0){posY=0;}//up
-    if(posX>(innerWidth-pacmanWidth)){posX=(innerWidth-pacmanWidth);}//right
-    if(posY>(innerHeight-pacmanHeight)){posY=(innerHeight-pacmanHeight);}//down
-    deplacement();
-};
-
-function deplacement(){
-    pacman.style.left=posX+"px";
-    pacman.style.top=posY+"px";
-    pacman.style.transform="scaleX("+directionX+") rotate("+angle+")";
-}
+function GenerateAnswer() {
+    responses = [
+        'It is certain',
+        'It is decidedly so',
+        'Without a doubt',
+        'Yes definitely',
+        'You may rely on it',
+        'As I see it, yes',
+        'Most likely',
+        'Outlook good',
+        'Yes',
+        'Signs point to yes',
+        'Reply hazy try again',
+        'Ask again later',
+        'Better not tell you now',
+        'Cannot predict now',
+        'Concentrate and ask again',
+        'Do not count on it',
+        'My reply is no',
+        'My sources say no',
+        'Outlook not so good',
+        'Very doubtful'
+    ];
+    response = responses[Math.floor(Math.random() * responses.length)];
+    document.getElementById('answer').innerHTML = response;
+  }
+ 
